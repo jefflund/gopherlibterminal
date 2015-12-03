f0125ee17681542f4c3e3b2cc5656f9d5c75e5f5
Michael Patraw <michaelpatraw@gmail.com>
2015-12-02 16:19:50 -0800

    Change test to be a rain demo.

--- glt_test.go
+++ glt_test.go
@@ -1,32 +1,80 @@
 package glt
 
 import "testing"
+import "math/rand"
 
-func TestEverything(t *testing.T) {
-	x, y := 0, 0
-	nx, ny := 0, 0
+func TestRain(t *testing.T) {
+	cols, lines := 0, 0
+	delay := 50
+
+	xpos, ypos := make([]int, 5), make([]int, 5)
 	Open()
 	defer Close()
 
+	cols = State(TkWidth) - 4
+	lines = State(TkHeight) - 4
+
+	for j := 4; j >= 0; j-- {
+		xpos[j] = rand.Intn(cols) + 2
+		ypos[j] = rand.Intn(lines) + 2
+	}
+
+	j := 0
 	for {
+		x := rand.Intn(cols) + 2
+		y := rand.Intn(lines) + 2
+		Color(0xffffffff)
+		Print(x, y, ".")
+
+		Print(xpos[j], ypos[j], "o")
+		if j == 0 {
+			j = 4
+		} else {
+			j--
+		}
+		Print(xpos[j], ypos[j], "O")
+		if j == 0 {
+			j = 4
+		} else {
+			j--
+		}
+		Color(0xff00007f)
+		Print(xpos[j], ypos[j]-1, "-")
+		Print(xpos[j]-1, ypos[j], "|.|")
+		Print(xpos[j], ypos[j]+1, "-")
+		if j == 0 {
+			j = 4
+		} else {
+			j--
+		}
 		Color(0xff0000ff)
-		Print(x, y, "@")
-		x, y = nx, ny
-		Color(0xffff0000)
-		Print(x, y, "@")
+		Print(xpos[j], ypos[j]-2, "-")
+		Print(xpos[j]-1, ypos[j]-1, "/ \\")
+		Print(xpos[j]-2, ypos[j], "| O |")
+		Print(xpos[j]-1, ypos[j]+1, "\\ /")
+		Print(xpos[j], ypos[j]+2, "-")
+		if j == 0 {
+			j = 4
+		} else {
+			j--
+		}
+		Print(xpos[j], ypos[j]-2, " ")
+		Print(xpos[j]-1, ypos[j]-1, "   ")
+		Print(xpos[j]-2, ypos[j], "     ")
+		Print(xpos[j]-1, ypos[j]+1, "   ")
+		Print(xpos[j], ypos[j]+2, " ")
+
+		xpos[j] = x
+		ypos[j] = y
 		Refresh()
 
-		inp := Read()
-		if inp == TkQ && Check(TkShift) || inp == TkClose {
-			break
-		} else if inp == TkLeft {
-			nx--
-		} else if inp == TkRight {
-			nx++
-		} else if inp == TkUp {
-			ny--
-		} else if inp == TkDown {
-			ny++
+		Delay(delay)
+
+		if HasInput() {
+			inp := Read()
+			if inp == TkQ && Check(TkShift) || inp == TkClose {
+				break
+			}
 		}
 	}
 }
